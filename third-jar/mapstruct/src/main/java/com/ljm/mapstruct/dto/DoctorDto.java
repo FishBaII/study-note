1339036bf6978195a96f81ee80cf56a7f1162cf2
lijianming <489442270@qq.com>
2023-05-21 02:40:46 +0800

    updated mapstruct project

--- third-jar/mapstruct/src/main/java/com/ljm/mapstruct/dto/DoctorDto.java
+++ third-jar/mapstruct/src/main/java/com/ljm/mapstruct/dto/DoctorDto.java
@@ -6,6 +6,24 @@ public class DoctorDto {
 
     private String specialty;
 
+    private String class1;
+
+    private String class2;
+
+    private String class3;
+
+    private String class4;
+
+    private String class5;
+
+    private String class6;
+
+    private String class7;
+
+    private String class8;
+
+    private String class9;
+
     public int getId() {
         return id;
     }
@@ -28,5 +46,77 @@ public class DoctorDto {
 
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
+    }
+
+    public String getClass1() {
+        return class1;
+    }
+
+    public void setClass1(String class1) {
+        this.class1 = class1;
+    }
+
+    public String getClass2() {
+        return class2;
+    }
+
+    public void setClass2(String class2) {
+        this.class2 = class2;
+    }
+
+    public String getClass3() {
+        return class3;
+    }
+
+    public void setClass3(String class3) {
+        this.class3 = class3;
+    }
+
+    public String getClass4() {
+        return class4;
+    }
+
+    public void setClass4(String class4) {
+        this.class4 = class4;
+    }
+
+    public String getClass5() {
+        return class5;
+    }
+
+    public void setClass5(String class5) {
+        this.class5 = class5;
+    }
+
+    public String getClass6() {
+        return class6;
+    }
+
+    public void setClass6(String class6) {
+        this.class6 = class6;
+    }
+
+    public String getClass7() {
+        return class7;
+    }
+
+    public void setClass7(String class7) {
+        this.class7 = class7;
+    }
+
+    public String getClass8() {
+        return class8;
+    }
+
+    public void setClass8(String class8) {
+        this.class8 = class8;
+    }
+
+    public String getClass9() {
+        return class9;
+    }
+
+    public void setClass9(String class9) {
+        this.class9 = class9;
     }
 }
